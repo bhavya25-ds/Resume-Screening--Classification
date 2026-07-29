2fb2c33cf5ebfebc2a13d7d2e7c40ef8e799d6fc
Bhavya <bhavyakumar.2506@gmail.com>
2026-07-30 01:13:45 +0530

    Created utility.py to contain all the main useful functions

---
+++ Utility.py
@@ -0,0 +1,8 @@
+"""
+Resume Screening/Classification: Utility function
+
+Contains the necessary functions for processing the data; 
+training, implementing, and evaluating the model; 
+along with comparing various models, and creating the confusion matrix.
+"""
+
