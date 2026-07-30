32c53f7694fa4ad76d1a8a45fbcf5077c68503cc
Bhavya <bhavyakumar.2506@gmail.com>
2026-07-30 15:37:45 +0530

    importing libs and stopwords in english language

--- Utility.py
+++ Utility.py
@@ -6,3 +6,21 @@ training, implementing, and evaluating the model;
 along with comparing various models, and creating the confusion matrix.
 """
 
+import re
+import numpy as np
+import pandas as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import (accuracy_score, precision_score, recall_score, f1_score, confusion_matrix)
+from sklearn.naive_bayes import MultinomialNB, ComplementNB
+from sklearn.linear_model import LogisticRegression, RidgeClassifier, PassiveAggressiveClassifier
+from sklearn.svm import LinearSVC
+
+try:
+    from nltk.corpus import stopwords
+    STOPWORDS= set(stopwords.words("english"))
+except Exception:
+    from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+    STOPWORDS= set(ENGLISH_STOP_WORDS)
+
+
